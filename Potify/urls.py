55569f8ad68c8754e0950d3682d271e56d40a960
nyshanbaev <bajtikovskidmitrij@gmail.com>
2023-02-23 11:48:26 +0600

    third

--- Potify/urls.py
+++ Potify/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -35,4 +37,4 @@ urlpatterns = [
     path('v1/api/feedback/', include('applications.feedback.urls')),
     
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
